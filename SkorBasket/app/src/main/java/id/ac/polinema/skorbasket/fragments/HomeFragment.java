c29fb0e166fcd3962c0faa54a564a3bdea52d524
BrilianFajar <fajar28021999@gmail.com>
2019-11-04 11:53:10 +0700

    jobsheet10 (SkorBasket)

--- SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/fragments/HomeFragment.java
+++ SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/fragments/HomeFragment.java
@@ -2,16 +2,15 @@ package id.ac.polinema.skorbasket.fragments;
 
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import id.ac.polinema.skorbasket.R;
 import id.ac.polinema.skorbasket.viewmodels.SharedScore;
